0e6d1e1cd87439abc7cd4240e03e5f6d623e5b92
cruwmorgan <morganokeke@gmail.com>
2023-08-24 04:35:07 -0700

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -34,8 +34,8 @@ class BasicCache(BaseCaching):
             Return:
                 value of the key
         """
-        if key or self.cache_data.get(key) is not None:
-            valuecache = self.cache_data.get(key)
-            return valuecache
-        else:
+        if key or self.cache_data.get(key) is None:
             return None
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
