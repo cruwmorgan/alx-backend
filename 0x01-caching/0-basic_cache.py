7f15430ba0b04248405cb81746a047cef08f4408
cruwmorgan <morganokeke@gmail.com>
2023-08-24 04:20:18 -0700

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    Basic dictionary
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+        a caching system
+        Arg;
+            BaseCaching - use self.cache_data - dictionary from the parent
+            class BaseCaching
+    """
+    def put(self, key, item):
+        """
+            Add an item to cache data
+
+            Args:
+                key: of the dict
+                item: value of the key
+        """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            Get an item by key from cache data
+
+            Args:
+                key: identifies the cache data
+
+            Return:
+                value of the key
+        """
+        if key is None or self.cache_data.get(key) is None:
+            return "None"
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
