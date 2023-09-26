06944f31847714d8678e890a018f7ed7238ea270
ctuwmorgan <morganokeke@gmail.com>
2023-09-26 07:51:55 +0000

    Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,27 +1,9 @@
+'use strict';
 // Import the required libraries using ES6 import syntax
 import redis from 'redis';
 
 // Create a Redis client
-const client = redis.createClient();
-
-// Set up event handlers for the client
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-});
-
-client.on('error', (error) => {
-  console.error(`Redis client not connected to the server: ${error}`);
-});
-
-// Attempt to connect to the Redis server
-client.ping((err, response) => {
-  if (err) {
-    console.error(`Redis client not connected to the server: ${err}`);
-  } else {
-    console.log('Redis server is running');
-  }
-
-  // Close the client connection
-  client.quit();
-});
-
+const client = redis.createClient()
+  // Set up event handlers for the client
+  .on('error', err => console.log(`Redis client not connected to the server: ${err}`))
+  .on('ready', () => console.log('Redis client connected to the server'));
